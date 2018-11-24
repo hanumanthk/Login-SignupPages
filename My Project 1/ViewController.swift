a23f39675554bb3eb7c278efb5383c6a9ff265c2
iOS Training <iostraining@ganeshs-macbook-pro.local>
2018-11-24 20:16:14 +0530

    Initial Commit

---
+++ My Project 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Project 1
+//
+//  Created by iOS Training on 11/24/18.
+//  Copyright © 2018 iOS Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
